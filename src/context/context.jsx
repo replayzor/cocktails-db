251ef86fc2f34e995e0a6a36f04005a3a2582410
replayzor <oltean.ionut1989@gmail.com>
2023-05-18 17:29:54 +0300

    Add fetchDrinks and state

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,13 +1,59 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import { createContext, useContext } from "react";
+import axios from "axios";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-	return <AppContext.Provider value={{ url }}>{children}</AppContext.Provider>;
+	const [loading, setLoading] = useState(true);
+	const [searchTerm, setSearchTerm] = useState("");
+	const [cocktails, setCocktails] = useState([]);
+
+	// fetchDrinks
+	const fetchDrinks = async () => {
+		setLoading(true);
+		try {
+			const response = await axios.get(`${url}${searchTerm}`);
+			const { drinks } = response.data;
+			if (drinks) {
+				const newCocktails = drinks.map((item) => {
+					const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+						item;
+
+					return {
+						id: idDrink,
+						name: strDrink,
+						image: strDrinkThumb,
+						info: strAlcoholic,
+						glass: strGlass,
+					};
+				});
+				setCocktails(newCocktails);
+			} else {
+				setCocktails([]);
+			}
+			setLoading(false);
+		} catch (error) {
+			console.log(error.message);
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		fetchDrinks();
+	}, []);
+
+	return (
+		<AppContext.Provider value={{ loading, cocktails }}>
+			{children}
+		</AppContext.Provider>
+	);
 };
 
 export const useGlobalContext = () => useContext(AppContext);
