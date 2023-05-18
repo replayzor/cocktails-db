3b75b3acc04fce3a2f6f7e365c69260b616ccc2e
replayzor <oltean.ionut1989@gmail.com>
2023-05-18 17:23:50 +0300

    Add error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
 	return (
-		<div>
-			<h2>error page</h2>
-		</div>
+		<section className="error-page section">
+			<div className="error-container">
+				<h1>opps! no page found</h1>
+				<Link to="/" className="btn btn-primary">
+					back home
+				</Link>
+			</div>
+		</section>
 	);
 };
 export default Error;
