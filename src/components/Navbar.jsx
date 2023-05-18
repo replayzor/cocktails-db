7faa010f9066509b99209a812271f1680e04bb5a
replayzor <oltean.ionut1989@gmail.com>
2023-05-18 17:25:22 +0300

    Add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
 	return (
-		<div>
-			<h2>navbar component</h2>
-		</div>
+		<nav className="navbar">
+			<div className="nav-center">
+				<Link to="/">
+					<span className="first-word">Cocktails</span>
+					<span className="second-word">Database</span>
+				</Link>
+				<ul className="nav-links">
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
 	);
 };
 export default Navbar;
