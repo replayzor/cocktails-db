45a1d44a7759aaf4cdf8cd172ae620d6c6e24cae
replayzor <oltean.ionut1989@gmail.com>
2023-05-18 17:25:49 +0300

    Add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,27 @@
+/* eslint-disable react/no-unescaped-entities */
 const About = () => {
 	return (
-		<div>
-			<h2>about page</h2>
-		</div>
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				"Welcome to Cocktails Database! We are passionate about cocktails and
+				dedicated to providing you with a comprehensive collection of drink
+				recipes and information. Our website utilizes the power of the{" "}
+				<a
+					href="https://www.thecocktaildb.com/"
+					target="_blank"
+					rel="noreferrer"
+					className="link"
+				>
+					CocktailDB API
+				</a>{" "}
+				to bring you a vast array of cocktail recipes from around the world.
+				Whether you're a mixology enthusiast, a bartender, or simply someone who
+				enjoys a good drink, you'll find inspiration and guidance here. Explore
+				our database, discover new cocktail creations, and learn the art of
+				mixing drinks. Cheers to endless possibilities!"
+			</p>
+		</section>
 	);
 };
 export default About;
