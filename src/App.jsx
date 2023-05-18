521d7f54f8b5d7daef76fea83092c1468ae58675
replayzor <oltean.ionut1989@gmail.com>
2023-05-18 17:24:57 +0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktails from "./pages/SingleCocktails";
+import Error from "./pages/Error";
+
+// components
+import Navbar from "./components/Navbar";
+
 function App() {
 	return (
-		<div>
-			<h1>app component</h1>
-		</div>
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="cocktail/:cocktailId" element={<SingleCocktails />} />
+				<Route path="*" element={<Error />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
