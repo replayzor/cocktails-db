8400b8ce5c76dbd8af61487c3cc43c5ab04248bd
replayzor <oltean.ionut1989@gmail.com>
2023-05-18 17:29:10 +0300

    Add single cocktail page

--- src/pages/SingleCocktails.jsx
+++ src/pages/SingleCocktails.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 const SingleCocktails = () => {
 	return (
-		<div>
-			<h2>single cocktail page</h2>
-		</div>
+		<section className="section cocktail-section">
+			<Link to="/" className="btn btn-primary">
+				back home
+			</Link>
+			<h2 className="section-title">cocktail name</h2>
+			<div className="drink">
+				<img src="" alt="" />
+				<div className="drink-info">{/* map over drink info */}</div>
+			</div>
+		</section>
 	);
 };
 export default SingleCocktails;
