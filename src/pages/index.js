376a13462ad98b9ef2e6161d8cd97a43501f1717
replayzor <oltean.ionut1989@gmail.com>
2023-06-01 16:05:20 +0300

    Export as default

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export { default as Home } from "./Home";
+export { default as Cocktail } from "../components/Cocktail";
+export { default as About } from "./About";
+export { default as Newsletter } from "./Newsletter";
+export { default as SingleCocktails } from "./SingleCocktails";
+export { default as RootLayout } from "./RootLayout";
+export { default as Error } from "./Error";
+export { default as SinglePageError } from "./SinglePageError";
