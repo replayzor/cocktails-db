50e316e53481e9274ce035b03631e9ddd53c7fa8
replayzor <oltean.ionut1989@gmail.com>
2023-06-02 16:01:53 +0300

    Add component

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,4 +1,68 @@
+import axios from "axios";
+import { Form, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const newsletterUrl = "https://www.course-api.com/cocktails-newsletter";
+
 const NewsletterPage = () => {
-	return <div>NewsletterPage</div>;
+	return (
+		<Form className="form" method="post">
+			<h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+				our newsletter
+			</h4>
+			{/* name */}
+			<div className="form-row">
+				<label htmlFor="name" className="form-label">
+					name
+				</label>
+				<input
+					type="text"
+					className="form-input"
+					name="name"
+					id="name"
+					defaultValue="Ionut"
+				/>
+			</div>
+			{/* last name */}
+			<div className="form-row">
+				<label htmlFor="lastName" className="form-label">
+					last name
+				</label>
+				<input
+					type="text"
+					className="form-input"
+					name="lastName"
+					id="lastName"
+					defaultValue="Oltean"
+				/>
+			</div>
+			{/* email */}
+			<div className="form-row">
+				<label htmlFor="email" className="form-label">
+					email
+				</label>
+				<input
+					type="email"
+					className="form-input"
+					name="email"
+					id="email"
+					defaultValue="test@test.com	"
+				/>
+			</div>
+			<button className="btn btn-block" style={{ marginTop: "0.5rem" }}>
+				submit
+			</button>
+		</Form>
+	);
 };
 export default NewsletterPage;
+
+export const action = async ({ request }) => {
+	const formData = await request.formData();
+	const data = Object.fromEntries(formData);
+
+	const response = await axios.post(newsletterUrl, data);
+	toast.success(response.data.msg);
+
+	return redirect("/");
+};
