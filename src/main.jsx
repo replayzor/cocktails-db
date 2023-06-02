f0e2d727b71c31f4f33a3d61ae765063b9a29bd3
replayzor <oltean.ionut1989@gmail.com>
2023-06-02 16:03:30 +0300

    Add toastify

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<QueryClientProvider client={queryClient}>
+		<ToastContainer position="top-center" autoClose={2000} />
 		<App />
 	</QueryClientProvider>
 );
